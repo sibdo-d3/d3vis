40d35fb7777971b47b52bfd97fffda26533a700b
Conrad Rider <cride5@crider.co.uk>
2015-09-14 10:32:12 +0700

    Update d3vis.js

--- d3vis.js
+++ d3vis.js
@@ -17,7 +17,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with SibD3.  If not, see <http://www.gnu.org/licenses/>.
  *
  *
  * @author Conrad Rider  <www.crider.co.uk>
